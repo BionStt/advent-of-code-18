a393e8b0f70010a1fa7491a1d45eea5d317558c1
Joshua Yates <josht.yates@btinternet.com>
2018-12-01 21:23:01 +0000

    Challenge 1 part 1

---
+++ challenge-1/challenge-1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter a file name: ");
+            var fileName = Console.ReadLine();
+            var lines = File.ReadLines(fileName);
+            var freq = 0;
+            foreach (var line in lines)
+            {
+                freq += int.Parse(line);
+            }
+            Console.WriteLine(freq);
+        }
+    }
+}
